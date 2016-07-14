34b575964af19faac0170ffe4e115381d468af02
Jerson Betin <jersonbetin@hotmail.com>
2016-07-14 11:21:29 -0500

    second commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+//app.js
+import React from 'react'  
+import ReactDOM from 'react-dom'
+
+import './styles.scss'
+
+const Hello = () => <h1>Hello world!</h1>
+
+ReactDOM.render(<Hello />, document.getElementById('root'))  
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// webpack.config.js
+
+var getConfig = require('hjs-webpack')
+
+module.exports = getConfig({  
+  in: 'src/app.js',
+  out: 'public',
+  clearBeforeBuild: true
+})
